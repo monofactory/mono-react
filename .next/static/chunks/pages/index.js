66a38160610ac076a807e127cfef4ca0d555d791
hwan-cheol  yong <21moru@gmail.com>
2023-02-02 11:36:58 +0900

    11

--- .next/static/chunks/pages/index.js
+++
@@ -1,9 +0,0 @@
-
-},
-/******/ function(__webpack_require__) { // webpackRuntimeModules
-/******/ var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
-/******/ __webpack_require__.O(0, ["pages/_app","main"], function() { return __webpack_exec__("./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?absolutePagePath=%2FUsers%2Fmoming%2Fworkspace%2Fhomepage%2Fmonofactory.github.io%2Fpages%2Findex.tsx&page=%2F!"); });
-/******/ var __webpack_exports__ = __webpack_require__.O();
-/******/ _N_E = __webpack_exports__;
-/******/ }
-]);
