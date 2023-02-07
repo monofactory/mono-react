67553f8753896d29295f2e2784c54bb6a749ac35
monofactory <21moru@gmail.com>
2023-02-07 05:54:32 +0000

    Deploying to gh-pages from @ monofactory/monofactory-react@687bc51462ea0b20066d30396d4af92705f56e23 🚀

---
+++ _next/static/lZ1NpSPdqImu8sfGKAMUj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b6bc9cbf960d5d0c.css","static/chunks/pages/index-07b52901f020fb21.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
