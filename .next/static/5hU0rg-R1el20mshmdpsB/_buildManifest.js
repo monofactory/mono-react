fcea5bd20ae35a22c658f2f351e4fbb77eddd425
hwan-cheol  yong <21moru@gmail.com>
2023-03-14 17:24:31 +0900

    수정

--- .next/static/5hU0rg-R1el20mshmdpsB/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b6bc9cbf960d5d0c.css","static/chunks/pages/index-91789435a72a190a.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
